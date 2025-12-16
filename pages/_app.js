ea1c451cb3656050d2ddaaf48c1d082a29a85e9d
Rloney86 <164127125+Rloney86@users.noreply.github.com>
2025-12-16 03:17:21 -0500

    Implement navigation bar in _app.js
    Added a navigation bar with links to the homepage and about page.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,26 @@
-import '../styles.css'
+import "../styles.css";
+import Link from "next/link";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <nav
+        style={{
+          maxWidth: 900,
+          margin: "0 auto",
+          padding: "16px 24px 0",
+          display: "flex",
+          gap: 14,
+          alignItems: "center",
+        }}
+      >
+        <Link href="/" style={{ fontWeight: 700 }}>
+          Booster Bowl
+        </Link>
+        <Link href="/about">About</Link>
+      </nav>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
